4716fbc52a6cff439453e79e5df1db0e7083b521
orfilip-sketch <orfilip@gmail.com>
2026-09-03 21:35:00 +0200

    Create MainActivity.kt

---
+++ app/src/main/java/com/example/autoclicker/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.autoclicker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(android.R.layout.simple_list_item_1)
+    }
+}
